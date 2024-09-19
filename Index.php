549d0ad3eee9ce6a0a78fe16c79aafb777ae52e5
Rafael <andrestimegame64@gmail.com>
2024-09-19 00:42:12 -0400

    segundo commit la wea

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Inicio Cambio</title>
     <link rel="stylesheet" href="build/StyleSheet/InicioCSS.css">
     <link rel="stylesheet" href="build/bootstrap-5.3.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="build/StyleSheet/HeaderCSS.css"><!--estilo header-->
