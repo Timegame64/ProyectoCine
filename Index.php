00a2b334498acc076c36759aab2dad855249dbb4
Rafael <andrestimegame64@gmail.com>
2024-09-22 23:24:46 -0400

    iniciooooo

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio Cambio AAAA</title>
+    <title>Inicioooooooooooooooooooooooo</title>
     <link rel="stylesheet" href="build/StyleSheet/InicioCSS.css">
     <link rel="stylesheet" href="build/bootstrap-5.3.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="build/StyleSheet/HeaderCSS.css"><!--estilo header-->
